3c5c60e476e128c9b7d6b5a036198d94f732328e
kracelh542 <kracelh542@gmail.com>
2024-01-08 09:34:16 +0100

    añadimos enunciado del proyecto

--- src/main/java/daw/ListasAleatorias.java
+++ src/main/java/daw/ListasAleatorias.java
@@ -13,4 +13,11 @@ public class ListasAleatorias {
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
+    
+    //En un programa nuevo, crea dos listas de tamaño aleatorio de letras 
+//    entre la 'a' y la 'z'. Estas listas deben tener entre 10 y 20 elementos. 
+//    Una vez creadas las listas, imprime por pantalla sus elementos usando 
+//    un foreach. Crea una tercera lista con los elementos de la primera 
+//    que no están en la segunda. Imprime el resultado usando API Stream y lambdas.
+   
 }
