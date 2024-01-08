0649eeda4a82e95c526c3fed503237cf1d464f5c
kracelh542 <kracelh542@gmail.com>
2024-01-08 09:30:30 +0100

    Comienzo del proyecto

---
+++ src/main/java/daw/ListasAleatorias.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package daw;
+
+/**
+ *
+ * @author khalid
+ */
+public class ListasAleatorias {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
