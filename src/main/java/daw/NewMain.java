7bfa930d4042ed6586ded190aebb18492ee97282
kracelh542 <kracelh542@gmail.com>
2024-01-09 10:27:23 +0100

    v2.1

---
+++ src/main/java/daw/NewMain.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package daw;
+
+/**
+ *
+ * @author khalid
+ */
+public class NewMain {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      
+        ListasAleatorias lista1y2 = new ListasAleatorias();
+        
+        System.out.println(lista1y2);
+        
+        
+        
+    }
+    
+}
